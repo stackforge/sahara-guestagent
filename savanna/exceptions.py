c1288c38b8511483d06edf1dad40d4aa52019343
Dmitry Mescheryakov <dmescheryakov@mirantis.com>
2014-02-19 19:26:16 +0400

    The initial commit
    Change-Id: I88cf5bb4c49b22293b0b299f182f55a4d179c877

---
+++ savanna/exceptions.py
@@ -0,0 +1,60 @@
+# Copyright (c) 2013 Mirantis Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+class SavannaException(Exception):
+    """Base Exception for the project
+
+    To correctly use this class, inherit from it and define
+    a 'message' and 'code' properties.
+    """
+    message = "An unknown exception occurred"
+    code = "UNKNOWN_EXCEPTION"
+
+    def __str__(self):
+        return self.message
+
+    def __init__(self):
+        super(SavannaException, self).__init__(
+            '%s: %s' % (self.code, self.message))
+
+
+class RemoteCommandException(SavannaException):
+    message = "Error during command execution: \"%s\""
+
+    def __init__(self, cmd, ret_code=None, stdout=None,
+                 stderr=None):
+        self.code = "REMOTE_COMMAND_FAILED"
+
+        stdout = stdout.decode('ascii', 'ignore')
+        stderr = stderr.decode('ascii', 'ignore')
+
+        self.cmd = cmd
+        self.ret_code = ret_code
+        self.stdout = stdout
+        self.stderr = stderr
+
+        self.message = self.message % cmd
+
+        if ret_code:
+            self.message += '\nReturn code: ' + str(ret_code)
+
+        if stderr:
+            self.message += '\nSTDERR:\n' + stderr
+
+        if stdout:
+            self.message += '\nSTDOUT:\n' + stdout
+
+        self.message = self.message.decode('ascii', 'ignore')
